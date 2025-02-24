cd94874f35d27c122a185af21c66279f8d5d3aa3
quaid5050 <inoxent5050@gmail.com>
2025-02-24 22:40:52 +0500

    add intial editor and get the update stae

--- src/package/App.tsx
+++ src/package/App.tsx
@@ -6,39 +6,42 @@
  *
  */
 
-import type {JSX} from 'react';
-
-import {$createLinkNode} from '@lexical/link';
-import {$createListItemNode, $createListNode} from '@lexical/list';
-import {LexicalComposer} from '@lexical/react/LexicalComposer';
-import {$createHeadingNode, $createQuoteNode} from '@lexical/rich-text';
+import type { JSX } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { $createLinkNode } from '@lexical/link';
+import { $createListItemNode, $createListNode } from '@lexical/list';
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { OnChangePlugin as LexicalOnChangePlugin } from '@lexical/react/LexicalOnChangePlugin';
+import { $createHeadingNode, $createQuoteNode } from '@lexical/rich-text';
 import {
   $createParagraphNode,
   $createTextNode,
   $getRoot,
   $isTextNode,
   DOMConversionMap,
+  EditorState,
   TextNode,
 } from 'lexical';
 
-import {isDevPlayground} from './appSettings';
-import {FlashMessageContext} from './context/FlashMessageContext';
-import {SettingsContext, useSettings} from './context/SettingsContext';
-import {SharedHistoryContext} from './context/SharedHistoryContext';
-import {ToolbarContext} from './context/ToolbarContext';
+import { isDevPlayground } from './appSettings';
+import { FlashMessageContext } from './context/FlashMessageContext';
+import { SettingsContext, useSettings } from './context/SettingsContext';
+import { SharedHistoryContext } from './context/SharedHistoryContext';
+import { ToolbarContext } from './context/ToolbarContext';
 import Editor from './Editor';
 import logo from './images/logo.svg';
 import PlaygroundNodes from './nodes/PlaygroundNodes';
 import DocsPlugin from './plugins/DocsPlugin';
 import PasteLogPlugin from './plugins/PasteLogPlugin';
-import {TableContext} from './plugins/TablePlugin';
+import { TableContext } from './plugins/TablePlugin';
 import TestRecorderPlugin from './plugins/TestRecorderPlugin';
-import {parseAllowedFontSize} from './plugins/ToolbarPlugin/fontSize';
+import { parseAllowedFontSize } from './plugins/ToolbarPlugin/fontSize';
 import TypingPerfPlugin from './plugins/TypingPerfPlugin';
 import Settings from './Settings';
 import PlaygroundEditorTheme from './themes/PlaygroundEditorTheme';
-import {parseAllowedColor} from './ui/ColorPicker';
-
+import { parseAllowedColor } from './ui/ColorPicker';
+import Button from './ui/Button';
+import { $generateHtmlFromNodes } from '@lexical/html';
 console.warn(
   'If you are profiling the playground app, please ensure you turn off the debug view. You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.',
 );
@@ -53,7 +56,7 @@ function $prepopulatedRichText() {
     quote.append(
       $createTextNode(
         `In case you were wondering what the black box at the bottom is – it's the debug view, showing the current state of the editor. ` +
-          `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`,
+        `You can disable it by pressing on the settings control in the bottom-left of your screen and toggling the debug view setting.`,
       ),
     );
     root.append(quote);
@@ -167,7 +170,7 @@ function buildImportMap(): DOMConversionMap {
           }
           const extraStyles = getExtraStyles(element);
           if (extraStyles) {
-            const {forChild} = output;
+            const { forChild } = output;
             return {
               ...output,
               forChild: (child, parent) => {
@@ -190,16 +193,16 @@ function buildImportMap(): DOMConversionMap {
 
 function App(): JSX.Element {
   const {
-    settings: {isCollab, emptyEditor, measureTypingPerf},
+    settings: { isCollab, emptyEditor, measureTypingPerf },
   } = useSettings();
 
   const initialConfig = {
     editorState: isCollab
       ? null
       : emptyEditor
-      ? undefined
-      : $prepopulatedRichText,
-    html: {import: buildImportMap()},
+        ? undefined
+        : $prepopulatedRichText,
+    html: { import: buildImportMap() },
     namespace: 'Playground',
     nodes: [...PlaygroundNodes],
     onError: (error: Error) => {
@@ -208,18 +211,53 @@ function App(): JSX.Element {
     theme: PlaygroundEditorTheme,
   };
 
+  const editorStateRef = useRef<EditorState | null>(null);
+  const initialEditorStateRef = useRef<EditorState | null>(null);
+
+  // Get editor initial state (e.g. loaded from backend)
+  const loadContent = async () => {
+    // 'empty' editor
+    const value = '{"root":{"children":[{"children":[],"direction":null,"format":"","indent":0,"type":"paragraph","version":1}],"direction":null,"format":"","indent":0,"type":"root","version":1}}';
+    return value as unknown as EditorState;
+  }
+
+
+  useEffect(() => {
+    const loadInitialEditorState = async () => {
+      const initialEditorState = await loadContent();
+      initialEditorStateRef.current = initialEditorState as EditorState;
+    }
+    loadInitialEditorState();
+  }, []);
+
+  const onChange = (editorState: EditorState) => {
+    editorStateRef.current = editorState;
+    console.log("editorStateRef", editorStateRef.current);
+  }
+
+
+  const onSubmit = () => {
+    console.log(JSON.stringify(editorStateRef.current, null, 2));
+    // const htmlString = $generateHtmlFromNodes(editor, null);
+    // console.log("htmlString", htmlString);
+  }
+
+
   return (
-    <LexicalComposer initialConfig={initialConfig}>
+    <LexicalComposer
+      initialConfig={{
+        ...initialConfig,
+        editorState: initialEditorStateRef.current
+      }}
+
+    >
       <SharedHistoryContext>
         <TableContext>
           <ToolbarContext>
-            <header>
-              <a href="https://lexical.dev" target="_blank" rel="noreferrer">
-                <img src={logo} alt="Lexical Logo" />
-              </a>
-            </header>
             <div className="editor-shell">
               <Editor />
+              <Button
+                onClick={onSubmit}>Save</Button>
             </div>
             <Settings />
             {isDevPlayground ? <DocsPlugin /> : null}
@@ -229,6 +267,7 @@ function App(): JSX.Element {
             {measureTypingPerf ? <TypingPerfPlugin /> : null}
           </ToolbarContext>
         </TableContext>
+        <LexicalOnChangePlugin onChange={onChange} />
       </SharedHistoryContext>
     </LexicalComposer>
   );
@@ -240,40 +279,6 @@ export default function PlaygroundApp(): JSX.Element {
       <FlashMessageContext>
         <App />
       </FlashMessageContext>
-      <a
-        href="https://github.com/facebook/lexical/tree/main/packages/lexical-playground"
-        className="github-corner"
-        aria-label="View source on GitHub">
-        <svg
-          width="80"
-          height="80"
-          viewBox="0 0 250 250"
-          style={{
-            border: '0',
-            color: '#eee',
-            fill: '#222',
-            left: '0',
-            position: 'absolute',
-            top: '0',
-            transform: 'scale(-1,1)',
-          }}
-          aria-hidden="true">
-          <path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z" />
-          <path
-            d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2"
-            fill="currentColor"
-            style={{
-              transformOrigin: '130px 106px',
-            }}
-            className="octo-arm"
-          />
-          <path
-            d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z"
-            fill="currentColor"
-            className="octo-body"
-          />
-        </svg>
-      </a>
     </SettingsContext>
   );
 }
